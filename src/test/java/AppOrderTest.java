873bcdc073417bdf8e117ee7b833ea9dbbb8a304
Adamizgoi <adamizgoi97@gmail.com>
2022-10-08 16:53:51 +0300

    Last AppOrderTest fix

--- src/test/java/AppOrderTest.java
+++ src/test/java/AppOrderTest.java
@@ -184,15 +184,15 @@ public class AppOrderTest {
     }
 
     @Test
-    /** Попытка отправки формы, где в поле ФИ содержится только одна буква (негативный кейс)
+    /** Отправка формы, где в поле ФИ содержится только одна буква
      */
     void shouldNotSendFormSuccessfullyIfInNameSurnameFieldHasNoSurnameAndNameHasOnlyOneLetter() {
-        driver.findElement(By.cssSelector("span[data-test-id= name] input")).sendKeys("С");
+        driver.findElement(By.cssSelector("span[data-test-id= name] input")).sendKeys("Ю");
         driver.findElement(By.cssSelector("span[data-test-id=\"phone\"] input")).sendKeys("+79060483535");
         driver.findElement(By.cssSelector("label[data-test-id=\"agreement\"] .checkbox__box")).click();
         driver.findElement(By.cssSelector("div[class*=\"form-field\"]:last-child button")).click();
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("p[data-test-id = \"order-success\"]")).getText().trim();
         assertEquals(expected, actual);
     }
 
