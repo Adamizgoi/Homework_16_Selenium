b6cb41337cd01b96995c3de3c8bf963cc7ee3108
Adamizgoi <adamizgoi97@gmail.com>
2022-10-08 15:30:16 +0300

    Fix testcases

--- src/test/java/AppOrderTest.java
+++ src/test/java/AppOrderTest.java
@@ -425,7 +425,7 @@ public class AppOrderTest {
     void shouldSendFormSuccessfullyIfNumberHaveNotMobileOperatorCode() {
         driver.findElement(By.cssSelector("span[data-test-id= name] input")).sendKeys("Федорова Светлана");
         driver.findElement(By.cssSelector("span[data-test-id=\"phone\"] input")).sendKeys("+74060483535");
-        driver.findElement(By.cssSelector("label[data-test-id=\"agreement\"] .checkbox__bx")).click();
+        driver.findElement(By.cssSelector("label[data-test-id=\"agreement\"] .checkbox__box")).click();
         driver.findElement(By.cssSelector("div[class*=\"form-field\"]:last-child button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("p[data-test-id = \"order-success\"]")).getText().trim();
